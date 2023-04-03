323543b7864b26119ebbe73868d4a3f9b9dd7349
raissaputra <raissaputra02@gmail.com>
2023-04-03 14:47:13 +0700

    insert data ke db

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//import body parser
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
+
+
 
 //import route posts
 const postsRouter = require('./routes/posts');
